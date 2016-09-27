9ca8d62834f386ed19c02101c18e02152dcd5075
Paul Hoehne <paul.hoehne@marklogic.com>
2016-09-27 13:55:50 -0400

    Fixing script files

--- scripts/load_rxnorm.bat
+++ scripts/load_rxnorm.bat
@@ -13,9 +13,9 @@ set ML_PORT=8700
 set ML_PERMISSIONS="rest-writer,update,rest-reader,read"
 set RXNORM_GRAPH="http://dvievr.health.dha.mil/semantics/rxnorm"
 
-mlcp import -input_file_path $RXNORM_DIR -input_file_type rdf -input_file_pattern ".*\.nt" ^
-    -output_graph $RXNORM_GRAPH -output_permissions $ML_PERMISSIONS ^
-    -host $ML_HOST -port $ML_PORT -username $ML_USERNAME -password $ML_PASSWORD
+mlcp import -input_file_path %RXNORM_DIR% -input_file_type rdf -input_file_pattern ".*\.nt" ^
+    -output_graph %RXNORM_GRAPH% -output_permissions %ML_PERMISSIONS% ^
+    -host %ML_HOST% -port %ML_PORT% -username %ML_USERNAME% -password %ML_PASSWORD%
 
 goto END
 
--- scripts/load_snomed.bat
+++ scripts/load_snomed.bat
@@ -13,9 +13,9 @@ set ML_PORT=8700
 set ML_PERMISSIONS="rest-writer,update,rest-reader,read"
 set SNOMED_GRAPH="http://dvievr.health.dha.mil/semantics/snomed"
 
-mlcp import -input_file_path $SNOMED_DIR -input_file_type rdf -input_file_pattern ".*\.nt" ^
-    -output_graph $SNOMED_GRAPH -output_permissions $ML_PERMISSIONS ^
-    -host $ML_HOST -port $ML_PORT -username $ML_USERNAME -password $ML_PASSWORD
+mlcp import -input_file_path %SNOMED_DIR% -input_file_type rdf -input_file_pattern ".*\.nt" ^
+    -output_graph %SNOMED_GRAPH% -output_permissions %ML_PERMISSIONS% ^
+    -host %ML_HOST% -port %ML_PORT% -username %ML_USERNAME% -password %ML_PASSWORD%
 
 goto END
 
